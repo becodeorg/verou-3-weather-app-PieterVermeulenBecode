57169a076814b4e0a0383ed503c9da3c240735f6
Pieter Vermeulen <pietermeulen@hotmail.com>
2022-02-01 11:04:28 +0100

    refactored getFirstFiveDays

--- script.js
+++ script.js
@@ -7,29 +7,28 @@ window.onload = ()=> {
 
 const startWeatherApp=(countryData)=>{
     createCountryList(countryData);   
-    document.getElementById("steden").onchange= () => {  
-        citiesChanged(countryData);      
-    }
-    document.getElementById("landen").onchange=()=>{         
-        createCityList(countryData);
-    }    
+    document.getElementById("steden").onchange= () => citiesChanged(countryData);    
+    document.getElementById("landen").onchange=()=>  createCityList(countryData);
 }
 
 const citiesChanged=(countryData)=>{
-    fetchApi(countryData);
-        const stad =getCity();
-        document.getElementById("titel").innerHTML="What is the weather in <span class='bold'>"+stad+"</span>"
-        fetchPicture();
+    fetchWeather(countryData);
+    fetchPicture();
+    changeTitle();
 }
+
+const changeTitle=()=>{
+    const stad =getCityName();
+    document.getElementById("titel").innerHTML="What is the weather in <span class='bold'>"+stad+"</span>"     
+}
+
 const createCityList=(countryData)=>{
     const landenLijst=document.getElementById("landen");  
     document.getElementById("steden").innerHTML="<option>Choose the city</option>";
    
    for(let i=0;i<countryData.length;i++){
-       let landNaam=countryData[i].country;       
-       
-        if(landenLijst.value==landNaam){        
-
+       let landNaam=countryData[i].country;   
+       if(landenLijst.value==landNaam){        
             for(let b=0;b<=countryData[i].cities.length;b++){
                 const newOption=document.createElement("option")
                 newOption.innerText=countryData[i].cities[b];
@@ -39,6 +38,7 @@ const createCityList=(countryData)=>{
         }
     }  
 }
+
 const getLandCode=(countryData)=>{        
     const landenLijst=document.getElementById("landen");    
     for(let i=0;i<countryData.length;i++){
@@ -52,7 +52,7 @@ const getLandCode=(countryData)=>{
 
 const fetchPicture=()=>{
     let count=0;
-    const stad=getCity();   
+    const stad=getCityName();   
    fetch("https://api.unsplash.com/search/photos?query=" + stad + "&client_id=" +Data.accesKey)
    .then(function(resp) { return resp.json() })
    .then(function(image) {
@@ -67,9 +67,9 @@ const fetchPicture=()=>{
     });
 }
 
-const fetchApi= async(countryData)=>{  
+const fetchWeather= async(countryData)=>{  
     const landCode=getLandCode(countryData);
-    const stad=getCity();   
+    const stad=getCityName();   
     fetch('https://api.openweathermap.org/data/2.5/forecast?q='+stad+','+landCode+'&APPID='+Data.key)  
     .then(function(resp) { return resp.json() })
     .then(function(data) {
@@ -101,21 +101,52 @@ const createCountryList=(countryData)=>{
     }        
 }
 
-const getCity=()=>{
-    const stedenLijst=document.getElementById("steden");    
-    return stedenLijst.value;
+const getCityName=()=>{
+    const cityList=document.getElementById("steden");    
+    return cityList.value;
+}
+const daysInMonth=(month,year)=>{    
+    console.log(new Date(year, month, 0).getDate());
+    return new Date(year, month, 0).getDate();
+}
+const nextDay=(dayPrevious,monthPrevious)=>{
+    let daymonth={
+        nextDay:"",
+        nextMonth:""
+    }
+    let daysInThisMonth=daysInMonth(monthPrevious,2022);
+    if (dayPrevious<daysInThisMonth)
+        {
+            daymonth.nextDay=dayPrevious+1;
+            daymonth.nextMonth=monthPrevious;
+    }else {
+        daymonth.nextDay=1;
+        daymonth.nextMonth=monthPrevious+1;
+    }
+    return daymonth;
+}
+
+const getFirstFiveDays=()=>{
+    let d=new Date();    
+    let day = d.getDate();    
+    let month=d.getMonth()+1;
+    
+    let firstFiveDays=[]
+    for (let i=0;i<5;i++){
+        firstFiveDays[i]=day+"/"+month
+        let daymonth=nextDay(day,month);
+        day=daymonth.nextDay;
+        month=daymonth.nextMonth;        
+    }
+    console.log(firstFiveDays);
+    return firstFiveDays;
 }
 
 const displayWeather=(data)=>{
     const voorspelZone=document.getElementById("voorspelling");
     voorspelZone.innerHTML="";
-    let d=new Date();    
-    let day = d.getDate();
-    
-    let month=d.getMonth()+1;
-     //TODO: when days are bigger then 31-> start at 1 again
-    let dagenVanDeWeek=[day+"/"+month,(day+1)+"/"+month,(day+2)+"/"+month,(day+3)+"/"+month,(day+4)+"/"+month,(day+5)+"/"+month,(day+6)+"/"+month];
-    
+    let dagenVanDeWeek=getFirstFiveDays();
+       
     for(let i=0;i<40;i=i+8){    
         const newDiv4=document.createElement("div");
         newDiv4.className="eenDag";
@@ -138,5 +169,3 @@ const displayWeather=(data)=>{
         newDiv4.appendChild(newDiv3);
     }
 }
-
-
